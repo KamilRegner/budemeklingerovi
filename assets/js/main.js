5e95d7bf153f5be47ea51cf2ae91addcd22aebbb
petrkucerak <petr.kucera@mainstream.cz>
2021-08-21 13:12:19 +0200

    Create simply animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
    menu();
+   gallery();
 });
 
 function menu() {
@@ -7,4 +8,18 @@ function menu() {
       $("#button_menu").toggleClass("active");
       $("#section_menu").toggleClass("active");
    });
+}
+
+function gallery() {
+   setInterval(() => {
+      let src = "/assets/image/gallery/0";
+      src += getRandomArbitrary(1, 9);
+      src += ".jpg";
+      document.getElementById('gallery').setAttribute("src", src);
+      console.log(src);
+   }, 4000);
+}
+
+function getRandomArbitrary(min, max) {
+   return Math.floor(Math.random() * (max - min) + min);
 }
